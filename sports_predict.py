a1de2c13f2777225407097a4e82dc2f0f87a9573
Neba Roland <53044894+Roland-coder@users.noreply.github.com>
2021-10-27 00:49:52 +0200

    Update sports_predict.py

--- sports_predict.py
+++ sports_predict.py
@@ -3,12 +3,13 @@ import numpy as np
 import pandas as pd
 import pickle
 from PIL import Image
+from smart_open import smart_open
 
 st.title("Sports Predict App")
 st.header("Sports Performace Prediction")
 st.write("This web app predicts the overall performance of a player based on particular features")
 
-model = pickle.load(open("https://mlassignment.s3.eu-de.cloud-object-storage.appdomain.cloud/final_model%20(1).save","rb"))
+model = pickle.load(smart_open('https://mlassignment.s3.eu-de.cloud-object-storage.appdomain.cloud/final_model%20(1).save', 'rb'))
 image = Image.open("sports.jpg")
 st.image(image, use_column_width=True)
 st.write("Please insert values to get overall prediction of player")
