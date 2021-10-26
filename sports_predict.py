14d3f03a9a356389dd49e1d80382c8f3dba3f292
Neba Roland <53044894+Roland-coder@users.noreply.github.com>
2021-10-26 22:29:40 +0200

    adding model features

--- sports_predict.py
+++ sports_predict.py
@@ -1,5 +1,32 @@
 import streamlit as st
 import numpy as np
+import pandas as pd
 
 st.title("Sports Predict App")
+st.header("Sports Performace Prediction")
 st.write("This web app predicts the overall performance of a player based on particular features")
+
+image = Image.open("sports.jpg")
+st.image(image, use_column_width=True, format='JPG')
+st.write("Please insert values to get overall prediction of player")
+
+potential = st.slider("Player Potential: ", 0, 100)
+club = st.number_input("Pick Club number based on club description above")
+wage = st.number_input("Please enter wage of player")
+international_reputation = st.slider("Player International Reputation: ", 0, 5)
+short_passing = st.slider("Player Short Passing: ", 0, 100)
+reactions = st.slider("Player Reactions: ", 0, 100)
+vision = st.slider("Player Vision: ", 0, 100)
+composure = st.slider("Player Composure: ", 0, 100)
+
+data = {'Potential' : potential,
+ 'Club' : club,
+ 'Wage' : wage,
+ 'International Reputation' : international_reputation,
+ 'ShortPassing' : short_passing,
+ 'Reactions' : reactions,
+ 'Vision' : vision,
+ 'Composure' : composure
+
+	}
+features = pd.DataFrame(data, index[0])
