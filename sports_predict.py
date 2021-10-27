1c3ae337e2f41875f5f6d68ac8d04d30dc901a28
Neba Roland <53044894+Roland-coder@users.noreply.github.com>
2021-10-27 09:16:04 +0200

    Update sports_predict.py

--- sports_predict.py
+++ sports_predict.py
@@ -35,8 +35,8 @@ data = {'Potential' : potential,
  'Composure' : composure
 
 	}
-model = pickle.load(open('final_model.save', 'rb'))
-# model = pickle.load(smart_open('https://mlassignment.s3.eu-de.cloud-object-storage.appdomain.cloud/final_model%20(1).save', 'rb'))
+# model = pickle.load(open('final_model.save', 'rb'))
+model = pickle.load(smart_open('https://mlassignment.s3.eu-de.cloud-object-storage.appdomain.cloud/final_model%20(1).save', 'rb'))
 
 
 features = pd.DataFrame(data, index=[0])
